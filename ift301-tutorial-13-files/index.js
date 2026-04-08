74ea5a118cb17fde7dda1cccc17ce92039d006e5
mikayla001 <svevee119tf@gmail.com>
2026-04-07 22:11:13 -0700

    unzipped folder contents

---
+++ ift301-tutorial-13-files/index.js
@@ -0,0 +1,14 @@
+
+
+
+/* Predefined Functions */
+
+function isStrictMode() {
+	return (eval("var __temp = null"), (typeof __temp === "undefined")) ? 'strict' : 'non-strict';
+}
+
+function functionExists(fname) {
+  const ctx = (typeof window !== 'undefined' ? window : global)
+  if(typeof fname === 'string') fname = ctx[fname]
+  return typeof fname === 'function'
+}
