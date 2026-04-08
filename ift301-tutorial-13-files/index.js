05702229038c313ba086e31498eda7bfde982162
mikayla001 <svevee119tf@gmail.com>
2026-04-07 22:55:14 -0700

    step 4 validate email function

--- ift301-tutorial-13-files/index.js
+++ ift301-tutorial-13-files/index.js
@@ -1,5 +1,21 @@
 "use strict";
 
+function validateEmail(email) {
+  let tmp = email.split('@');
+
+  //ends functiohn if there is no @ symbol
+  if (tmp.length < 2) return "missing the @ symbol";
+
+  prefixPattern = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")/
+
+  if (!prefixPattern.test(tmp[0])) return "invalid prefix";
+
+  domainPattern = /(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+  if (!domainPattern.test(tmp[1])) return "invalid domain";
+
+  return "valid";
+}
 
 /* Predefined Functions */
 
